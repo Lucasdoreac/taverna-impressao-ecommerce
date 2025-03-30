836aef052a6647fdef1259d94f501ffd491b812e
Lucas Dórea Cardoso <160977944+Lucasdoreac@users.noreply.github.com>
2025-03-30 11:57:00 -0300

    Update: Adicionar funções para lazy loading de imagens com URL e imagens de fundo

--- app/helpers/AssetOptimizerHelper.php
+++ app/helpers/AssetOptimizerHelper.php
@@ -6,6 +6,7 @@
  * - Minificação e combinação de arquivos CSS e JavaScript
  * - Geração de URLs com versão para controle de cache
  * - Compressão de assets estáticos
+ * - Lazy loading de imagens e imagens de fundo
  */
 class AssetOptimizerHelper {
     /**
@@ -269,6 +270,52 @@ class AssetOptimizerHelper {
     }
     
     /**
+     * Retorna uma tag de imagem com atributos para lazy loading a partir de uma URL completa
+     * 
+     * @param string $imageUrl URL completa da imagem
+     * @param string $alt Texto alternativo
+     * @param string $class Classes CSS adicionais
+     * @param array $attributes Atributos adicionais
+     * @return string Tag de imagem com lazy loading
+     */
+    public static function lazyImageUrl($imageUrl, $alt = '', $class = '', $attributes = []) {
+        self::init();
+        
+        // Gerar placeholder de baixa qualidade ou SVG
+        $placeholder = BASE_URL . 'assets/images/placeholder.svg';
+        
+        // Construir atributos adicionais
+        $attributesStr = '';
+        foreach ($attributes as $key => $value) {
+            $attributesStr .= ' ' . $key . '="' . htmlspecialchars($value) . '"';
+        }
+        
+        // Tag de imagem com lazy loading
+        return '<img src="' . $placeholder . '" data-src="' . $imageUrl . '" alt="' . htmlspecialchars($alt) . '" class="lazy ' . $class . '"' . $attributesStr . '>';
+    }
+    
+    /**
+     * Retorna um elemento div com background-image e lazy loading
+     * 
+     * @param string $imageUrl URL da imagem
+     * @param string $class Classes CSS adicionais
+     * @param array $attributes Atributos adicionais
+     * @return string Tag div com lazy loading para background image
+     */
+    public static function lazyBackgroundImage($imageUrl, $class = '', $attributes = []) {
+        self::init();
+        
+        // Construir atributos adicionais
+        $attributesStr = '';
+        foreach ($attributes as $key => $value) {
+            $attributesStr .= ' ' . $key . '="' . htmlspecialchars($value) . '"';
+        }
+        
+        // Elemento div com lazy loading
+        return '<div class="lazy-bg ' . $class . '" data-bg="' . $imageUrl . '"' . $attributesStr . '></div>';
+    }
+    
+    /**
      * Minifica conteúdo CSS
      * 
      * @param string $css Conteúdo CSS a ser minificado
@@ -282,7 +329,7 @@ class AssetOptimizerHelper {
         $css = preg_replace('/\s+/', ' ', $css);
         
         // Remover espaços antes e depois de {:;,}
-        $css = preg_replace('/\s*([\{\}:;,])\s*/', '$1', $css);
+        $css = preg_replace('/\s*([{}:;,])\s*/', '$1', $css);
         
         // Remover ponto-e-vírgula desnecessário antes de fechar chaves
         $css = preg_replace('/;}/', '}', $css);
@@ -333,7 +380,7 @@ class AssetOptimizerHelper {
             $url = $matches[1];
             
             // Ignorar URLs absolutas ou data:
-            if (preg_match('/^(https?:|data:|\/)/i', $url)) {
+            if (preg_match('/^(https?:|data:|\/)/', $url)) {
                 return "url({$url})";
             }
             
